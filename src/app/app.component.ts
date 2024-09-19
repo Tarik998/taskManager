ae3a98e52585e91785657dd4712123b80a41b545
Tarik Mehmedovic <tarik.mehmedovic@virginpulse.com>
2024-09-19 12:40:09 +0200

    Modules added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {HeaderComponent} from "./header/header.component";
-import {UserComponent} from "./user/user.component";
 import {DUMMY_USERS} from "./dummy-user";
-import {TasksComponent} from "./tasks/tasks.component";
-import {NgFor, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
